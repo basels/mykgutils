0befbf85856bf2bd963ba5b42a4a1caffc929305
Basel Shbita <basel921@gmail.com>
2020-02-16 17:53:09 -0800

    modify repo to be a package

---
+++ baselutils/__init__.py
@@ -0,0 +1,2 @@
+from .mykgutils import fclrprint, slugify, get_num_of_files_in_dir, get_num_of_lines_in_file, \
+        bytes_to_human, seconds_to_human, download_file, column_num2str
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="baselutils",
+    version="0.0.1",
+    author="Basel Shbita",
+    author_email="basel921@gmail.com",
+    description="A package for utility functions we like using",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/basels/mykgutils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
