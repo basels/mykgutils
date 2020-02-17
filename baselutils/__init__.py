8dc4cdc295fb53f1b16b9485929bffd8fef42fc2
Basel Shbita <basel921@gmail.com>
2020-02-16 17:57:56 -0800

    mykgutils ready as a package

--- baselutils/__init__.py
+++ baselutils/__init__.py
@@ -1,2 +1,2 @@
 from .mykgutils import fclrprint, slugify, get_num_of_files_in_dir, get_num_of_lines_in_file, \
-        bytes_to_human, seconds_to_human, download_file, column_num2str
+        bytes_to_human, seconds_to_human, download_file, column_num2str, hash_string_md5
